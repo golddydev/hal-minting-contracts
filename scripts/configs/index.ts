dcdf39a7cf25200410a3f21319c5060a14d8ddfe
Golddy Dev <firingdev@gmail.com>
2025-09-06 01:50:17 +0800

    fix: enable mainnet in scripts

--- scripts/configs/index.ts
+++ scripts/configs/index.ts
@@ -3,11 +3,12 @@
 
 import { NetworkName } from "@helios-lang/tx-utils";
 
+import * as MAINNET_CONFIGS from "./mainnet.config.js";
 import * as PREPROD_CONFIGS from "./preprod.config.js";
 import * as PREVIEW_CONFIGS from "./preview.config.js";
 
 const GET_CONFIGS = (network: NetworkName) => {
-  if (network == "mainnet") throw new Error("Mainnet not configured yet");
+  if (network == "mainnet") return MAINNET_CONFIGS;
   if (network == "preprod") return PREPROD_CONFIGS;
   return PREVIEW_CONFIGS;
 };
